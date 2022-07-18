81adaca04544354c5dbb8effc87278db1a000d96
Sam <74021826+x39OME@users.noreply.github.com>
2022-07-18 17:37:53 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,48 @@
+/* global $, alert, console */
+// Header
+$(function () {
+  'use strict';
+// Adjust Slider Height
+  var winH    = $(window).height(),
+      navH    = $('.navbar').innerHeight();
+  $('.slider, .carousel-item').height(winH + navH);
+});
+// Scroll To Top
+$(function (){
+  'use strict';
+  // Scroll To Top Button
+  $(window).scroll(function(){
+    $('.block').each(function(){
+      if ($(window).scrollTop() > $(this).offset().top) {
+        }
+    });
+    var scrollToTop = $('.scroll-to-top')
+      if ($(window).scrollTop() >= 650) {
+        if (scrollToTop.is(':hidden')) {
+          scrollToTop.fadeIn(500);
+        }
+        } else {
+          scrollToTop.fadeOut(500);
+        }
+  });
+  // Clock On Scroll To Top Go Up
+  $('.scroll-to-top').click(function(event){
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000);
+  });
+});
+// Nice Scroll
+/*
+$(function () {
+  'use strict';
+  // Trigger Nice Scroll Plugin
+  $('html').niceScroll({
+  
+    cursorcolor: '#C9A958',
+    cursorwidth: 15,
+    cursorborderradius: 5,
+    cursorborder: '2px solid #C9A958'
+  });});
+*/
